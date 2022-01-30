b92a302666a245ae6cdcf83b01db2ff6015e090b
Hactually Benji <mrbenamiller@gmail.com>
2022-01-30 14:00:38 -0800

    Added dynamic label for info toggle

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import './Course.css';
 
 const Course = (props) => {
     const course = props.course;
     const topics = course.topics.map(element => <li key={element.code}>{element}</li>);
     const description = course.description;
     const [show, setShow] = useState(false);
+    const [buttonValue, setButtonValue] = useState('More');
+    console.log(buttonValue);
 
     const handleClick = () => {
         setShow(!show);
+        if (buttonValue == 'More') {
+            setButtonValue('Less');
+        } else {
+            setButtonValue('More');
+        }
+
     }
 
     return (
         <div className="Course">
-            <input type="submit" value="More" onClick={handleClick} />
             <h3>{course.code}: {course.title}</h3>
+            <input type="submit" value={buttonValue} onClick={handleClick} />
         { show ? <div>
             <h4>{description}</h4>
             <ul>
