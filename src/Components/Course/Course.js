c3a3b1c179679412a6ee53ad5a31be206f2d99be
Hactually Benji <mrbenamiller@gmail.com>
2022-01-30 13:50:30 -0800

    Added drop-down for course info

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -1,18 +1,26 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
 
 const Course = (props) => {
     const course = props.course;
     const topics = course.topics.map(element => <li key={element.code}>{element}</li>);
     const description = course.description;
+    const [show, setShow] = useState(false);
+
+    const handleClick = () => {
+        setShow(!show);
+    }
 
     return (
         <div className="Course">
+            <input type="submit" value="More" onClick={handleClick} />
             <h3>{course.code}: {course.title}</h3>
+        { show ? <div>
             <h4>{description}</h4>
             <ul>
                 {topics}
             </ul>
+            </div> : null }
         </div>
     );
 };
