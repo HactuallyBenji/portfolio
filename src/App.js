ce219612bcc65fd225686373390b0686862b99ac
Hactually Benji <mrbenamiller@gmail.com>
2022-02-24 19:53:00 -0800

    Placeholder

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import Video from './Components/Video/Video';
 function App() {
   return (
     <div className="App">
-      <Nav page="Home    " />
-      <div className="video-section">
-          <Video src="https://www.youtube.com/embed/kA9Pdu-IblI" />
-          <Video src="https://www.youtube.com/embed/XH84pIwzLCA" />
-      </div>
+      <p>Coming soon...</p>
+      <p><a href='http://hactuallybenji.com'>HactuallyBenji.com in the meantime</a></p>
     </div>
   );
 }
