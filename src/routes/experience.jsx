890e73cd5d2bc0b8d0443a4844ce819d8d9f810f
Hactually Benji <mrbenamiller@gmail.com>
2022-02-24 19:44:29 -0800

    Fixed experience page route

---
+++ src/routes/experience.jsx
@@ -0,0 +1,12 @@
+import './experience.css';
+import Nav from '../Components/Nav/Nav.js';
+
+const Experience = () => {
+    return (
+        <div className="Experience">
+            <Nav page="Experience"/>
+        </div>
+    );
+};
+
+export default Experience;
