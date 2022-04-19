eb34a46d5cce5db5d8e884c1f8857730471ae964
Hactually Benji <mrbenamiller@gmail.com>
2022-04-18 17:44:08 -0700

    Added project component

---
+++ src/Components/Project/Project.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useState } from 'react';
+import './Course.css';
+
+const Course = (props) => {
+    const course = props.course;
+    const topics = course.topics.map(element => <li key={element.code}>{element}</li>);
+    const description = course.description;
+    // const grade = course.grade;
+    const [show, setShow] = useState(false);
+    const [buttonValue, setButtonValue] = useState('More');
+    console.log(buttonValue);
+
+    const handleClick = () => {
+        setShow(!show);
+        if (buttonValue === 'More') {
+            setButtonValue('Less');
+        } else {
+            setButtonValue('More');
+        }
+
+    }
+
+    return (
+        <div className="content">
+            <div className="Course">
+                <div className="Course-Heading">
+                    <h3>{course.code}: {course.title} - {course.grade}</h3>
+                </div>
+                <button type="button" value={buttonValue} onClick={handleClick} >{buttonValue}</button>
+            { show ? <div className="more-info">
+                <h4>{description}</h4>
+                <ul>
+                    {topics}
+                </ul>
+                </div> : null }
+            </div>
+        </div>
+    );
+};
+
+export default Course;
